4f10add0aa51db1aa3bcb6e3fc63c076c86d9e52
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-01-07 21:12:31 +0530

    update

---
+++ leetcode/01.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool isprime(int p)
+  {
+    if (p<=1)
+    returnfalse 
+    st=sqrt(p);
+
+    for(i=2;i<+st;i++)
+    if(p%i==0)
+    return false;
+
+  }
+  return true;
+
+  np=0
+
+  r=numsixe();
+  c=num[0].sixe;
+
+
+
+  // digahonal 
+  for(i=0;i<r;i++)
+  {
+    if(isprime(num[i][i]))
+
+    mp=std::max(mp,num[i][i];
+    
+    
+    
+    // antidiagona;
+    if (isprime(num[i][r-1-i])))
+
+    mp=std::max(mp,num[i][i]);
+  }
+}

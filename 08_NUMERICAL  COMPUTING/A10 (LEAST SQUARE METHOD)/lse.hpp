81e5956d91b97067a294a09a9ef240d7309e17ae
bhushanzade02 <bhushanzade02@gmail.com>
2025-04-26 22:53:13 +0530

    Chebyshev Polynomial

--- 08_NUMERICAL  COMPUTING/A10 (LEAST SQUARE METHOD)/lse.hpp
+++ 08_NUMERICAL  COMPUTING/A10 (LEAST SQUARE METHOD)/lse.hpp
@@ -10,9 +10,12 @@ public:
     double sumx = 0, sumx2 = 0, sumx3 = 0, sumx4 = 0;
     double sumy = 0, sumxy = 0, sumyx2 = 0;
 
-
     int line(int x);
     int parabola(int x);
+
+
+
+    
     int exponential(int x);
     int powerfit(int x);
     double Tchebyshev(int k, double x);

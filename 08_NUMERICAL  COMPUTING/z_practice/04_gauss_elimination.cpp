03c873305b20b31dcb69974459f4fe3b368209c7
bhushanzade02 <bhushanzade02@gmail.com>
2025-05-02 17:09:05 +0530

    Forward Elimination

--- 08_NUMERICAL  COMPUTING/z_practice/04_gauss_elimination.cpp
+++ 08_NUMERICAL  COMPUTING/z_practice/04_gauss_elimination.cpp
@@ -53,6 +53,7 @@ int main() {
         }
     }
 
+    // Display Upper Triangular Matrix
     cout << "\nAfter Forward Elimination:\n";
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j <= n; ++j)

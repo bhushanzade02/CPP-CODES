31700a712d73a4733ff0bc96fbec9221bab2c5c1
bhushanzade02 <bhushanzade02@gmail.com>
2025-06-15 12:32:20 +0530

    'Update'

---
+++ demo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout<<"enter a score";
+    int score;
+    cin>>score;
+
+    if(score<=85) cout<<"failed";
+    if(score>=85) cout<<"passed";
+
+    return 0;
+
+
+}

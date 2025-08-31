842b505a6109e89ca6180faa37004c4e1b5e3d35
Bhushan Zade <bhushanzade02@gmail.com>
2025-08-31 21:59:14 +0530

    Update

--- 13_PARALLEL_COMPUTING_HPC(HIGH PERFORMANCE COMPUTING)/comm5.cpp
+++ 13_PARALLEL_COMPUTING_HPC(HIGH PERFORMANCE COMPUTING)/comm5.cpp
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    
     int n = 100;
     int part = n / size;
     int remainder = n % size;  

e7466a46ebbe3ce4d420268eade50a7e73ef1b6b
Bhushan Zade <bhushanzade02@gmail.com>
2025-09-05 16:36:03 +0530

    fix bugs

--- 13_PARALLEL_COMPUTING_HPC(HIGH PERFORMANCE COMPUTING)/prac/01.cpp
+++ 13_PARALLEL_COMPUTING_HPC(HIGH PERFORMANCE COMPUTING)/prac/01.cpp
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
     vector<int> A;
     int n = 100;
     int part = n / size;
+    int remainder = n % size;
 
     if (rank == 0)
     {
@@ -25,11 +26,15 @@ int main(int argc, char *argv[])
 
         for (int p = 1; p < size; p++)
         {
-            MPI_Send(A.data() + p * part, part, MPI_INT, p, 1, MPI_COMM_WORLD);
+            int count = (p < remainder) ? (part + 1) : (part);
+           int start_idx = p * part + min(p, remainder);
+
+            MPI_Send(A.data() + start_idx, count, MPI_INT, p, 1, MPI_COMM_WORLD);
         }
 
         int lsum = 0;
-        for (int i = 0; i < part; i++)
+        int rank0_count = (0 < remainder) ? (part + 1) : (part);
+        for (int i = 0; i < rank0_count; i++)
         {
             lsum += A[i];
         }
@@ -39,7 +44,7 @@ int main(int argc, char *argv[])
         {
             int sum;
             MPI_Recv(&sum, 1, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
-           
+
             total_sum += sum;
         }
 
@@ -48,16 +53,20 @@ int main(int argc, char *argv[])
 
     if (rank != 0)
     {
-        vector<int> B(part);
-        MPI_Recv(B.data(), part, MPI_INT, 0,1, MPI_COMM_WORLD, &status);
-    
-        int rank1_sum = 0;
-        for (int i = 0; i < part; i++)
+
+        int count = (rank < remainder) ? (part + 1) : part;
+
+        vector<int> B(count);
+
+        MPI_Recv(B.data(), count, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+
+        int local_sum = 0;
+        for (int i = 0; i < count; i++)
         {
-            rank1_sum += B[i];
+            local_sum += B[i];
         }
-      
-        MPI_Send(&rank1_sum, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+
+        MPI_Send(&local_sum, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
